328a085d490dd632b58bce77997dbd2a9657f69a
marciliodevjava <marciliodevjava@gmail.com>
2023-09-06 20:20:46 -0300

    Create FormatadorUtils

---
+++ src/main/java/br/com/rinha/rinha/utils/FormatadorUtils.java
@@ -0,0 +1,24 @@
+package br.com.rinha.rinha.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class FormatadorUtils {
+    public LocalDate dataStringDate(String nacimento) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate dataNascimento = LocalDate.parse(nacimento, formatter);
+            return dataNascimento;
+        } catch (Exception e) {
+            // Por exemplo: return null;
+            throw new IllegalArgumentException("Formato de data inválido. Use o formato yyyy-MM-dd");
+        }
+    }
+
+    public String dataDateString(LocalDate nascimento) {
+        return nascimento.toString();
+    }
+}
