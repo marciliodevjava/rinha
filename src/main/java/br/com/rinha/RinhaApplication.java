1411fd913789194cd65d3c2a9a70f0b5a3b95a02
marciliodevjava <marciliodevjava@gmail.com>
2023-09-11 20:22:22 -0300

    Update RinhaApplication

--- src/main/java/br/com/rinha/RinhaApplication.java
+++ src/main/java/br/com/rinha/RinhaApplication.java
@@ -2,8 +2,10 @@ package br.com.rinha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class RinhaApplication {
 
     public static void main(String[] args) {
