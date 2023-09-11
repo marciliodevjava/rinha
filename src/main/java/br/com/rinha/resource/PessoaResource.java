2bf67b4dd0457761e39fa6c28064739ab8c55fc7
marciliodevjava <marciliodevjava@gmail.com>
2023-09-11 20:23:03 -0300

    Update PessoasResource

--- src/main/java/br/com/rinha/resource/PessoaResource.java
+++ src/main/java/br/com/rinha/resource/PessoaResource.java
@@ -2,6 +2,7 @@ package br.com.rinha.resource;
 
 import br.com.rinha.dto.request.PessoaDto;
 import br.com.rinha.dto.response.PessoaRetornoDto;
+import br.com.rinha.exception.ErroBuscarIdSeguroException;
 import br.com.rinha.service.PessoaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,24 @@ public class PessoaResource {
         return ResponseEntity.created(uri).body(dto);
     }
 
-    @GetMapping
-    public ResponseEntity<PessoaRetornoDto> buscar(@PathVariable String nome, @PathVariable List<String> seguros){
+    @GetMapping("/{id}")
+    public ResponseEntity<PessoaRetornoDto> buscar(@PathVariable String id){
+        PessoaRetornoDto retorno = pessoaService.buscarPessoaId(id);
+        return ResponseEntity.ok(retorno);
+    }
 
-        return null;
+    @GetMapping
+    public ResponseEntity<List<PessoaRetornoDto>> buscarSeguros(@RequestParam(required = false, name = "id") String id,
+                                                               @RequestParam(required = false, name = "seguros") String seguros){
+        List<PessoaRetornoDto> retorno;
+        if (id != null){
+            retorno = pessoaService.buscarPessoaIdList(id);
+            return ResponseEntity.ok(retorno);
+        } else if (seguros != null){
+            retorno = pessoaService.buscarPessoaSeguros(seguros);
+            return ResponseEntity.ok(retorno);
+        } else {
+            throw new ErroBuscarIdSeguroException();
+        }
     }
 }
