afaf1920766429d480fd474a5157305652f34e7a
marciliodevjava <marciliodevjava@gmail.com>
2023-09-11 20:21:49 -0300

    Update MensagemEnum

--- src/main/java/br/com/rinha/enuns/MensagemEnum.java
+++ src/main/java/br/com/rinha/enuns/MensagemEnum.java
@@ -1,10 +1,14 @@
 package br.com.rinha.enuns;
 
 public enum MensagemEnum {
-    ERRO_CPF_DUPLICADO_EXCEPTION("Esse CPF já constra cadastrado");
+    ERRO_CPF_DUPLICADO_EXCEPTION("Esse CPF já constra cadastrado"),
+    ERRO_METODO_INVALIDO_EXCEPTION("Requisição inválida do tipo: "),
+    ERRO_CORPO_SOLICITACAO_AUXENTE_EXCEPTION("Corpo da solicitação necessário está ausente"),
+    ERRO_BUSCAR_ID_SEGUROS_EXCEPTION("Campo id e seguros sem nenhum parametro valido");
 
     private final String mensagem;
-    MensagemEnum(String mensagem){
+
+    MensagemEnum(String mensagem) {
         this.mensagem = mensagem;
     }
 
