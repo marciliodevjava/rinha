2e60fa8a6bbc23391bb0702f0b86766ec8f1db03
marciliodevjava <marciliodevjava@gmail.com>
2023-09-11 20:22:00 -0300

    Update PessoaDto

--- src/main/java/br/com/rinha/dto/request/PessoaDto.java
+++ src/main/java/br/com/rinha/dto/request/PessoaDto.java
@@ -1,8 +1,11 @@
 package br.com.rinha.dto.request;
 
+import br.com.rinha.annotations.interfaces.CPFouCNPJ;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+
 
 import java.util.List;
 
@@ -10,7 +13,10 @@ import java.util.List;
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PessoaDto {
+    @NotBlank(message = "Campo nome não poder ser nulo")
     private String nome;
+    @NotBlank(message = "Campo cpfCnpj não poder ser nulo")
+    @CPFouCNPJ
     private String cpfCnpj;
     private String nacimento;
     private List<String> seguros;
