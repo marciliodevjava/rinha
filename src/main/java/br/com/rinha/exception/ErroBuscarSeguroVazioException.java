b725f2a8e90a52d01870252400f929d9304baacf
marciliodevjava <marciliodevjava@gmail.com>
2023-09-18 18:12:07 -0300

    Create ErroBuscarSeguroVazioException

---
+++ src/main/java/br/com/rinha/exception/ErroBuscarSeguroVazioException.java
@@ -0,0 +1,16 @@
+package br.com.rinha.exception;
+
+public class ErroBuscarSeguroVazioException extends RuntimeException {
+
+    public ErroBuscarSeguroVazioException(){
+        super();
+    }
+
+    public ErroBuscarSeguroVazioException(String mensagem){
+        super(mensagem);
+    }
+
+    public ErroBuscarSeguroVazioException(String mensagem, Throwable couse){
+        super(mensagem, couse);
+    }
+}
