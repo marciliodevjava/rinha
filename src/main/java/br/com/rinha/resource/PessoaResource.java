57cd4f9cdbb7124eff4b472886548c3f37dd9836
marciliodevjava <marciliodevjava@gmail.com>
2023-09-18 18:39:32 -0300

    Update PessoaResource

--- src/main/java/br/com/rinha/resource/PessoaResource.java
+++ src/main/java/br/com/rinha/resource/PessoaResource.java
@@ -4,6 +4,8 @@ import br.com.rinha.dto.request.PessoaDto;
 import br.com.rinha.dto.response.PessoaRetornoDto;
 import br.com.rinha.exception.ErroBuscarIdSeguroException;
 import br.com.rinha.service.PessoaService;
+import br.com.rinha.utils.ValidadeSeguro;
+import br.com.rinha.utils.ValidadorUuid;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,10 @@ public class PessoaResource {
 
     @Autowired
     private PessoaService pessoaService;
+    @Autowired
+    private ValidadorUuid validadorUuid;
+    @Autowired
+    private ValidadeSeguro validadeSeguro;
 
     @PostMapping
     public ResponseEntity<PessoaRetornoDto> salvar(@RequestBody @Valid PessoaDto pessoaDto, UriComponentsBuilder builder) {
@@ -34,14 +40,16 @@ public class PessoaResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<PessoaRetornoDto>> buscarSeguros(@RequestParam(required = false, name = "id") String id,
-                                                                @RequestParam(required = false, name = "seguros") String seguros) {
+    public ResponseEntity<List<PessoaRetornoDto>> buscarSeguros(@RequestParam (required = true, name = "t") String termo) {
+
         List<PessoaRetornoDto> retorno;
-        if (id != null) {
-            retorno = pessoaService.buscarPessoaIdList(id);
+        boolean validaTermno = validadorUuid.isValidUUID(termo);
+        boolean validaSeguro = validadeSeguro.validadeSeguro(termo);
+        if (validaTermno == true) {
+            retorno = pessoaService.buscarPessoaIdList(termo);
             return ResponseEntity.ok(retorno);
-        } else if (seguros != null) {
-            retorno = pessoaService.buscarPessoaSeguros(seguros);
+        } else if (validaSeguro == true) {
+            retorno = pessoaService.buscarPessoaSeguros(termo);
             return ResponseEntity.ok(retorno);
         } else {
             throw new ErroBuscarIdSeguroException();
