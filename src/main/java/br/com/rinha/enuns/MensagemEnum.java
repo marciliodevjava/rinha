8b431c46f484dcd42236118d5425f21cf4dcfd9c
marciliodevjava <marciliodevjava@gmail.com>
2023-09-18 18:39:51 -0300

    Update MensagemEnum

--- src/main/java/br/com/rinha/enuns/MensagemEnum.java
+++ src/main/java/br/com/rinha/enuns/MensagemEnum.java
@@ -2,13 +2,14 @@ package br.com.rinha.enuns;
 
 public enum MensagemEnum {
 
-    ERRO_CPF_DUPLICADO_EXCEPTION("Esse CPF já constra cadastrado"),
+    ERRO_CPF_DUPLICADO_EXCEPTION("Esse CPF já constra cadastrado."),
     ERRO_METODO_INVALIDO_EXCEPTION("Requisição inválida do tipo: "),
-    ERRO_CORPO_SOLICITACAO_AUXENTE_EXCEPTION("Corpo da solicitação necessário está ausente"),
-    ERRO_BUSCAR_ID_SEGUROS_EXCEPTION("Campo id e seguros sem nenhum parametro valido"),
-    ERRO_ID_NAO_EXCEPTION("Sem dados nessa consulta"),
+    ERRO_CORPO_SOLICITACAO_AUXENTE_EXCEPTION("Corpo da solicitação necessário está ausente."),
+    ERRO_BUSCAR_ID_SEGUROS_EXCEPTION("Campo id e seguros sem nenhum parametro valido."),
+    ERRO_ID_NAO_EXCEPTION("Sem dados nessa consulta."),
     ERRO_UUID_IVALIDO_EXCEPTION("O id da solicitação não é um id válido."),
-    ERRO_SEGURO_IVALIDO_EXCEPTION("Não existe seguros com esse nome em nossa base de dados");
+    ERRO_SEGURO_IVALIDO_EXCEPTION("Não existe seguros com esse nome em nossa base de dados."),
+    ERRO_TERMO_IVALIDO_EXCEPTION("Informar t é obrigatório.");
 
     private final String mensagem;
 
