067357a7f5efedf37d8dad511f90aaf86cafd12e
marciliodevjava <marciliodevjava@gmail.com>
2023-09-19 20:44:58 -0300

    Update PessoaMapper

--- src/main/java/br/com/rinha/mapper/PessoaMapper.java
+++ src/main/java/br/com/rinha/mapper/PessoaMapper.java
@@ -8,6 +8,7 @@ import br.com.rinha.utils.GeradorUuidUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -47,8 +48,35 @@ public class PessoaMapper {
         return null;
     }
 
+    public PessoaRetornoDto mapearPessoaRetornoDto(Pessoas retorno) {
+        PessoaRetornoDto dto = new PessoaRetornoDto();
+        if (retorno != null) {
+            Pessoas pessoas = retorno;
+            dto.setId(pessoas.getId());
+            dto.setNome(pessoas.getNome());
+            dto.setCpfCnpj(pessoas.getCpfCnpj());
+            dto.setNacimento(formatadorUtils.dataDateString(pessoas.getNascimento()));
+            dto.setSeguros(this.verificaSeguro(pessoas.getSeguros()));
+            return dto;
+        }
+        return null;
+    }
+
     private List<String> verificaSeguro(List<String> seguros) {
         if (seguros.isEmpty()) return null;
         return seguros;
+    }
+
+    public List<PessoaRetornoDto> mapearPessoaListRetorno(List<Pessoas> pessoas) {
+        List<PessoaRetornoDto> listDto = new ArrayList<>();
+        if (Objects.nonNull(pessoas)) {
+            pessoas.forEach(pes -> {
+                PessoaRetornoDto dto = new PessoaRetornoDto();
+                dto = this.mapearPessoaRetornoDto(pes);
+                listDto.add(dto);
+            });
+            return listDto;
+        }
+        return null;
     }
 }
