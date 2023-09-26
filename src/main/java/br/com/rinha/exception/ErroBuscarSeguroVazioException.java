e1c0f10119bdad47592dcfad462d2bb4d7c0af51
marciliodevjava <marciliodevjava@gmail.com>
2023-09-25 22:52:15 -0300

    Update Identação ErroBuscarSeguroVazioException

--- src/main/java/br/com/rinha/exception/ErroBuscarSeguroVazioException.java
+++ src/main/java/br/com/rinha/exception/ErroBuscarSeguroVazioException.java
@@ -2,15 +2,15 @@ package br.com.rinha.exception;
 
 public class ErroBuscarSeguroVazioException extends RuntimeException {
 
-    public ErroBuscarSeguroVazioException(){
+    public ErroBuscarSeguroVazioException() {
         super();
     }
 
-    public ErroBuscarSeguroVazioException(String mensagem){
+    public ErroBuscarSeguroVazioException(String mensagem) {
         super(mensagem);
     }
 
-    public ErroBuscarSeguroVazioException(String mensagem, Throwable couse){
+    public ErroBuscarSeguroVazioException(String mensagem, Throwable couse) {
         super(mensagem, couse);
     }
 }
