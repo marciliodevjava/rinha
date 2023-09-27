d41ba153850bf061d620772f9fcb0d8050c27231
marciliodevjava <marciliodevjava@gmail.com>
2023-09-26 22:34:28 -0300

    Update Erro

--- src/main/java/br/com/rinha/exception/ErroSalvarPessoaException.java
+++ src/main/java/br/com/rinha/exception/ErroSalvarPessoaException.java
@@ -1,5 +1,9 @@
 package br.com.rinha.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ErroSalvarPessoaException extends RuntimeException {
 
     public ErroSalvarPessoaException() {
